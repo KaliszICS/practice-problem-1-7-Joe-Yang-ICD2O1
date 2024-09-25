b7b2e967c46c7a9a7a5f2efde8cb9b02e8321888
Joe-Yang-ICD2O1 <joe.yang@student.tdsb.on.ca>
2024-09-25 14:55:23 +0000

    answer

--- PP1_7.py
+++ PP1_7.py
@@ -1,19 +1,38 @@
 
 
-q1():
+def q1():
+  #Write code here
+  bool1= True
+  print(bool1)
+
+def q2():
   #Write code here
 
-q2():
+  bool1= input("Input an integer")
+  bool1= (bool1 > 5)
+  print(bool1)
+
+def q3():
   #Write code here
 
-q3():
+  bool1= input("Input a letter a")
+  bool1= (bool1 == "a")
+  print(bool1)
+
+def q4():
+  #Write code here
+  bool1= input("Input a word earlier in the dictionary than google: ")
+  bool1= (bool1 > "google")
+  print(bool1)
+
+def q5():
   #Write code here
 
-q4():
-  #Write code here
+  bool1= int(input("Input a integer"))
+  bool2= int(input("Input another integer"))
+  bool1= (bool1 * bool2)
+  print(bool1 > 40)
 
-q5():
-  #Write code here
 
 #Do edit the code below
 #Comment the lines below when running your tests
